33cf81a75977e9c51b6dc6b719d7752ad014a4e9
Tody <tody411@gmail.com>
2016-03-15 15:09:52 +0900

    Update setup script.

---
+++ pydrive_ex/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+## @package pydrive_ex.__init__
+#
+#  pydrive_ex.__init__ utility package.
+#  @author      tody
+#  @date        2016/03/15
+
+__author__ = 'tody'
+__version__ = '1.0'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+## @package setup
+#
+#  setup utility package.
+#  @author      tody
+#  @date        2016/03/15
+
+from setuptools import setup, find_packages
+from pydrive_ex import __author__, __version__, __license__
+
+setup(
+        name = 'pydrive_ex',
+        version = __version__,
+        description = 'Simple PyDrive Extension.',
+        license = __license__,
+        author = __author__,
+        url = 'https://github.com/tody411/PyDriveEx.git',
+        packages = find_packages(),
+        install_requires = ['google-api-python-client', 'PyDrive'],
+        )
